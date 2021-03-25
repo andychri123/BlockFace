10dba157e64c1274ea7375f5291ced00cc9feeaa
andychri123 <67240680+andychri123@users.noreply.github.com>
2021-03-25 13:59:02 +0000

    Update Message.sol

--- contracts/Message.sol
+++ contracts/Message.sol
@@ -4,30 +4,6 @@ import  {Owned} from "./Owned.sol";
 
 
 
-//                    ,
-//                   dM
-//                   MMr
-//                  4MMML                  .
-//                  MMMMM.                xf
-//  .              "M6MMM               .MM-
-//   Mh..          +MM5MMM            .MMMM
-//   .MMM.         .MMMMML.          MMMMMh          
-//    )MMMh.        MM5MMM         MMMMMMM                 ECCLESIASTES 3:8 KJV
-//     3MMMMx.     'MMM3MMf      xnMMMMMM" 
-//     '*MMMMM      MMMMMM.     nMMMMMMP"
-//       *MMMMMx    "MMM5M\    .MMMMMMM=
-//        *MMMMMh   "MMMMM"   JMMMMMMP
-//          MMMMMM   GMMMM.  dMMMMMM            .
-//           MMMMMM  "MMMM  .MMMMM(        .nnMP"
-//..          *MMMMx  MMM"  dMMMM"    .nnMMMMM*
-// "MMn...     'MMMMr 'MM   MMM"   .nMMMMMMM*"
-//  "4MMMMnn..   *MMM  MM  MMP"  .dMMMMMMM""
-//    ^MMMMMMMMx.  *ML "M .M*  .MMMMMM**"
-//       *PMMMMMMhn. *x > M  .MMMM**""
-//          ""**MMMMhx/.h/ .=*"
-//                   .3P"%....
-//                 nP"     "*MMnx
-
 contract Message {
 
     struct message {
